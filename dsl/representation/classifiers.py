f68408c7003c18a68c3c7960cc56a8a154dfc5e9
Judit Acs <judit@sch.bme.hu>
2015-06-02 17:41:50 +0200

    simple classifier: choose nth feature as the label

--- dsl/representation/classifiers.py
+++ dsl/representation/classifiers.py
@@ -4,11 +4,25 @@ from sklearn.naive_bayes import GaussianNB
 class BaseClassifier(object):
 
     @staticmethod
-    def lookup_classifier(cl_name):
+    def lookup_classifier(cl_name, *args, **kwargs):
         if isinstance(cl_name, str):
             if cl_name.lower() == 'naive_bayes':
                 return NaiveBayesClassifier()
+            if cl_name.lower() == 'simple':
+                return SimpleClassifier(*args, **kwargs)
         raise ValueError('Unknown classifier: {}'.format(cl_name))
+
+
+class SimpleClassifier(BaseClassifier):
+
+    def __init__(self, field=0):
+        self.field = field
+
+    def train(self, model, target):
+        pass
+
+    def classify_vector(self, vector):
+        return vector[self.field]
 
 
 class NaiveBayesClassifier(BaseClassifier):
