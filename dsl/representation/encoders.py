3025d6c0293d4f291c5399f391bf8769f7f000a0
László Grad-Gyenge <laszlo.grad-gyenge@creo.hu>
2015-06-02 15:57:14 +0200

    Added RBMEncoder to encoders.py

--- dsl/representation/encoders.py
+++ dsl/representation/encoders.py
@@ -1,4 +1,5 @@
 from PCA import PcaEncoder
+from RBM import RbmEncoder
 
 
 class BaseEncoder(object):
@@ -8,4 +9,6 @@ class BaseEncoder(object):
         if isinstance(enc_name, str):
             if enc_name.lower() == 'pca':
                 return PcaEncoder(*args, **kwargs)
+            if enc_name.lower() == 'rbm':
+                return RbmEncoder(*args, **kwargs)
         raise ValueError('Unknown encoder: {}'.format(enc_name))
