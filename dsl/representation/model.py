89e9bcd9a29d2630cea3587949e51ac8a30d9b2b
Judit Acs <judit@sch.bme.hu>
2015-06-04 15:50:14 +0200

    separate encoder and classifier arguments in Representation constructor

--- dsl/representation/model.py
+++ dsl/representation/model.py
@@ -5,8 +5,15 @@ from classifiers import BaseClassifier
 class Representation(object):
 
     def __init__(self, encoder, classifier, *args, **kwargs):
-        self.encoder = BaseEncoder.lookup_encoder(encoder, **kwargs)
-        self.classifier = BaseClassifier.lookup_classifier(classifier)
+        enc_args = {}
+        cl_args = {}
+        for k, v in kwargs.iteritems():
+            if k.startswith(encoder):
+                enc_args[k.lstrip(encoder + '_')] = v
+            elif k.startswith(classifier):
+                cl_args[k.lstrip(classifier + '_')] = v
+        self.encoder = BaseEncoder.lookup_encoder(encoder, **enc_args)
+        self.classifier = BaseClassifier.lookup_classifier(classifier, **cl_args)
 
     def encode(self, matrix):
         self.encoder.train(matrix)
