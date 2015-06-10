affc11b0d40ac7dd0b8ca84c6d5ab391ad90423c
László Grad-Gyenge <laszlo.grad-gyenge@creo.hu>
2015-06-10 18:15:29 +0200

    Fixed PCA encoder

--- dsl/representation/PCA.py
+++ dsl/representation/PCA.py
@@ -14,7 +14,7 @@ class PcaEncoder(object):
         #print(self.model.explained_variance_ratio_)
 
     def encode(self, vector):
-        return self.model.transform(vector)
+        return self.model.transform(vector.transpose()).transpose()
 
     @property
     def repr_model(self):
@@ -22,7 +22,12 @@ class PcaEncoder(object):
 
 
 if __name__ == '__main__':
+    N = 18000
+    obs = numpy.matrix(numpy.random.random((10,N))).transpose()
     encoder = PcaEncoder(dimension=2)
-    encoder.train(numpy.array([[1,1,1,0,0,0],[1,0,1,0,0,0],[1,1,1,0,0,0],[0,0,1,1,1,0], [0,0,1,1,0,0],[0,0,1,1,1,0]]))
-    encoded=encoder.encode(numpy.array([1,1,1,1,1,1]))
+    #encoder.train(numpy.array([[1,1,1,0,0,0],[1,0,1,0,0,0],[1,1,1,0,0,0],[0,0,1,1,1,0], [0,0,1,1,0,0],[0,0,1,1,1,0]]))
+    #encoded=encoder.encode(numpy.array([1,1,1,1,1,1]))
+    encoder.train(obs)
+    tobs = obs[1, :].transpose()
+    encoded=encoder.encode(tobs)
     print(encoded)
