5a94bdb88a89cb85ebc31bcbe07f90491843df82
Judit Acs <judit@sch.bme.hu>
2015-06-23 17:09:00 +0200

    matrix shapes fixed

--- dsl/representation/run_experiment.py
+++ dsl/representation/run_experiment.py
@@ -28,10 +28,15 @@ def main():
     lang_map = {i: fn for i, fn in enumerate(sorted(listdir(args.lang_map)))}
     if args.train.endswith('.mtx'):
         mtx = mmread(args.train).todense()
+        t_mtx = mmread(args.test).todense()
     else:
         with open(args.train) as stream:
             mtx = np.loadtxt(stream, np.float64)
-    labels = mtx[:, 0]
+        with open(args.test) as stream:
+            t_mtx = np.loadtxt(stream, np.float64)
+    labels = np.ravel(mtx[:, 0])
+    test_labels = t_mtx[:, 0]
+    test_mtx = t_mtx[:, 1:]
     if args.scale:
         train = scale(mtx[:, 1:], with_mean=False)
     else:
@@ -51,20 +56,18 @@ def main():
     logging.info('Model trained')
     acc = 0
     N = 0
-    with open(args.test) as f:
-        for l in f:
-            fs = l.strip().split()
-            lab = int(fs[0])
-            vec = np.array(map(float, fs[1:]))
-            cl = r.classify_vector(vec)
-            try:
-                guess = int(cl[0])
-            except IndexError:
-                guess = int(cl + 0.5)
-            N += 1
-            if int(guess) == int(lab):
-                acc += 1
-            print('{0}\t{1}'.format(lang_map[guess], lang_map[int(lab)]))
+    for vec_ in test_mtx:
+        vec = np.ravel(vec_)
+        cl = r.classify_vector(vec)
+        try:
+            guess = int(cl[0])
+        except IndexError:
+            guess = int(cl + 0.5)
+        lab = test_labels[N, 0]
+        N += 1
+        if int(guess) == int(lab):
+            acc += 1
+        print('{0}\t{1}'.format(lang_map[guess], lang_map[int(lab)]))
     print(float(acc) / N)
 
 if __name__ == '__main__':
